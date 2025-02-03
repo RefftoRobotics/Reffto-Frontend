f86cdc2a41d0cd098fb56490afd4ca8cc359a20a
Harsh Kumar <harshforcoding@gmail.com>
2025-02-03 22:55:38 +0530

    refactor Carousel component for improved code readability and formatting

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const images = [
-    "https://robu.in/wp-content/uploads/2022/11/Lithium-Ion-Battery-1-2.jpg",
-    "https://robu.in/wp-content/uploads/2022/11/Drone-kits.jpg",
-    "https://robu.in/wp-content/uploads/2022/11/Sensors.jpg",
+  "https://robu.in/wp-content/uploads/2022/11/Lithium-Ion-Battery-1-2.jpg",
+  "https://robu.in/wp-content/uploads/2022/11/Drone-kits.jpg",
+  "https://robu.in/wp-content/uploads/2022/11/Sensors.jpg",
 ];
 
 export default function Carousel() {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 2000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000);
 
-        return () => clearInterval(interval);
-    }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <div className="flex items-center justify-center my-4">
-            <img
-                src={images[currentIndex]}
-                alt="carousel"
-                className="mx-4 w-full h-64 object-cover rounded-lg"
-            />
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center my-4">
+      <img
+        src={images[currentIndex]}
+        alt="carousel"
+        className="mx-4 w-full h-64 object-cover rounded-lg"
+      />
+    </div>
+  );
 }
